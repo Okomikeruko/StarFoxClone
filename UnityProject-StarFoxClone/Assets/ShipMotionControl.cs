435ce3fd4295603d305ae1782e486dc32d39eba7
Lee Whittaker <lee@21zero.com>
2014-12-09 16:54:57 -0700

    Ship Build complete

---
+++ UnityProject-StarFoxClone/Assets/ShipMotionControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMotionControl : MonoBehaviour {
+
+	[SerializeField]
+	private float Speed, Pitch = 1, Roll = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.velocity = transform.forward * Speed;
+		rigidbody.rotation *= Quaternion.Euler(Input.GetAxis("Vertical") * Pitch, 0, -Input.GetAxis("Horizontal") * Roll);
+//		rigidbody.rotation *= Quaternion.Euler(-Input.acceleration.z * Pitch, 0, -Input.acceleration.x * Roll);
+
+
+		if( Input.GetMouseButtonDown (0))
+		{
+			// Fire
+		}
+	}
+
+
+}
