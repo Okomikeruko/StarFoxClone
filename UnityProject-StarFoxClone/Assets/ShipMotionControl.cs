2bcb62ee29c606415ee3f0cfccfd989b10238149
Lee Whittaker <lee@21zero.com>
2015-01-07 22:53:51 -0700

    Changed Accelerometer to Gyroscope motion control. Added Yaw to steering control Changed Walls to stop auto killing ship.

--- UnityProject-StarFoxClone/Assets/ShipMotionControl.cs
+++ UnityProject-StarFoxClone/Assets/ShipMotionControl.cs
@@ -2,30 +2,31 @@
 using System.Collections;
 
 public class ShipMotionControl : MonoBehaviour {
-
+	
 	[SerializeField]
-	private float Speed, acceleration, Pitch = 1, Roll = 1, maxHealth = 150;
+	private float Speed, acceleration, Pitch = 1, Roll = 1, Yaw = 1, maxHealth = 150;
 	[SerializeField]
 	private ParticleSystem Destruction;
 	[SerializeField]
 	public GameObject Addons, PointLight, blasterFire;
-
+	
 	private MasterGUI masterGUI;
 	private Vector3 pos;
-	private float x, z;
-
+	private float x, y, z;
+	private Quaternion offset;
+	
 	public float currentHealth { get; set; }
-
+	
 	void Start() {
 		currentHealth = maxHealth;
 		masterGUI = GameObject.Find ("MasterGUI").GetComponent<MasterGUI>();
 		pos = transform.position;
-	#if UNITY_IPHONE
-		x = Input.acceleration.x;
-		z = Input.acceleration.z;
-	#endif
+		#if UNITY_IPHONE
+		Input.gyro.enabled = true;
+		offset = Input.gyro.attitude;
+		#endif
 	}
-
+	
 	void Update () {
 		if(currentHealth > maxHealth){
 			currentHealth = maxHealth;
@@ -33,29 +34,31 @@ public class ShipMotionControl : MonoBehaviour {
 		masterGUI.distanceCovered += Vector3.Distance(pos, transform.position);
 		pos = transform.position;
 		rigidbody.velocity = transform.forward * Speed;
-
-	#if UNITY_EDITOR
+		
+		#if UNITY_EDITOR
 		rigidbody.rotation *= Quaternion.Euler(Input.GetAxis("Vertical") * Pitch, 0, -Input.GetAxis("Horizontal") * Roll);
-	#endif
-
-	#if UNITY_IPHONE
-		rigidbody.rotation *= Quaternion.Euler(
-			(z - Input.acceleration.z) * Pitch,
-			0,
-			(x - Input.acceleration.x) * Roll);
-	#endif	
+		#endif
+		
+		#if UNITY_IPHONE
+		x = Mathf.DeltaAngle(offset.eulerAngles.y, Input.gyro.attitude.eulerAngles.y) / 36; 
+		y = Mathf.DeltaAngle(offset.eulerAngles.x, Input.gyro.attitude.eulerAngles.x) / 36;
+		z = Mathf.DeltaAngle(offset.eulerAngles.z, Input.gyro.attitude.eulerAngles.z) / 36;
+		
+		rigidbody.rotation *= Quaternion.Euler(x * Pitch, y * Yaw, z * Roll);
+		
+		#endif	
 		Speed += acceleration * Time.deltaTime;
-
+		
 		if( Input.GetMouseButtonDown (0)){
 			Fire();
 		}
-
+		
 		if(currentHealth <= 0){
 			StartCoroutine(SelfDestruct());
 		}
 		masterGUI.health = currentHealth/maxHealth;
 	}
-
+	
 	void OnCollisionEnter(Collision col) {
 		if (col.transform.root.gameObject.tag == "Damage"){
 			currentHealth -= col.transform.root.gameObject.GetComponent<CollisionDamage>().collisionDamage;
@@ -63,17 +66,17 @@ public class ShipMotionControl : MonoBehaviour {
 				col.transform.root.gameObject.GetComponent<health>().currentHealth = 0;
 			}
 		}
-
+		
 		if (currentHealth > 0) {
 			GameObject.Find ("Shield").GetComponent<Sheild>().Hit();
 		}
 	}
-
+	
 	void Fire() {
 		GameObject fire = Instantiate(blasterFire, transform.position + (transform.forward * 30), transform.rotation) as GameObject;
 		fire.rigidbody.velocity = transform.forward * Speed * 3;
 	}
-
+	
 	IEnumerator SelfDestruct() {
 		Speed = acceleration = 0;
 		rigidbody.angularVelocity = Vector3.zero;
